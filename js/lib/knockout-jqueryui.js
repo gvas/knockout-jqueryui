2ce1e865e42d9be2c426286edcd2678334386223
Vas Gábor <gvas.munka@gmail.com>
2013-01-22 16:03:21 +0100

    Documented the spinner binding.

--- js/lib/knockout-jqueryui.js
+++ js/lib/knockout-jqueryui.js
@@ -192,7 +192,7 @@
                 $headers = $(element).find(headerSelector);
                 value.active($headers.index(ui.newHeader));
             });
-            /*jslint unparam:true*/
+            /*jslint unparam:false*/
         }
 
         //handle disposal
@@ -289,10 +289,10 @@
 
         var value = valueAccessor();
 
-        if (ko.isObservable(value.isOpen)) {
+        if (value.isOpen) {
             ko.computed({
                 read: function () {
-                    if (value.isOpen()) {
+                    if (ko.utils.unwrapObservable(value.isOpen)) {
                         $(element).dialog('open');
                     } else {
                         $(element).dialog('close');
@@ -405,6 +405,48 @@
         options: ['animate', 'disabled', 'max', 'min', 'orientation', 'range', 'step',
             'value', 'values'],
         events: ['create', 'start', 'slide', 'change', 'stop'],
+        postInit: postInit
+    });
+}());
+(function () {
+    'use strict';
+
+    var postInit;
+
+    postInit = function (element, valueAccessor) {
+        /// <summary>Keeps the value binding property in sync with the spinner's value.
+        /// </summary>
+        /// <param name='element' type='DOMNode'></param>
+        /// <param name='valueAccessor' type='Function'></param>
+
+        var value = valueAccessor();
+
+        if (value.value) {
+            ko.computed({
+                read: function () {
+                    $(element).spinner('value', ko.utils.unwrapObservable(value.value));
+                },
+                disposeWhenNodeIsRemoved: element
+            });
+        }
+
+        if (ko.isWriteableObservable(value.value)) {
+            $(element).on('spinchange.ko', function () {
+                value.value($(element).spinner('value'));
+            });
+        }
+
+        //handle disposal
+        ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+            $(element).off('ko');
+        });
+    };
+
+    ko.jqueryui.bindingFactory.create({
+        name: 'spinner',
+        options: ['culture', 'disabled', 'icons', 'incremental', 'max', 'min',
+            'numberFormat', 'page', 'step'],
+        events: ['create', 'start', 'spin', 'stop', 'change'],
         postInit: postInit
     });
 }());
