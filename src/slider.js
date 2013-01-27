3f5be1fa146c3c311c06fe3e21ee30d447c95892
Vas Gábor <gvas.munka@gmail.com>
2013-01-27 12:40:20 +0100

    Fixed the slider binding's live value parameter. Removed the slider binding's live values parameter as there is no reliable way to implement it.

--- src/slider.js
+++ src/slider.js
@@ -5,8 +5,8 @@
     var postInit;
 
     postInit = function (element, valueAccessor) {
-        /// <summary>Keeps the active binding property in sync with the tabs' state.
-        /// </summary>
+        /// <summary>Keeps the value and the values binding property in sync with the
+        /// slider widget's values.</summary>
         /// <param name='element' type='DOMNode'></param>
         /// <param name='valueAccessor' type='Function'></param>
 
@@ -15,15 +15,12 @@
         if (ko.isWriteableObservable(value.value)) {
             /*jslint unparam:true*/
             $(element).on('slidechange.ko', function (ev, ui) {
-                value.value(ui.value);
+                var $handles = $(element).find('.ui-slider-handle');
+                if ($handles[0] === ui.handle) {
+                    value.value(ui.value);
+                }
             });
             /*jslint unparam:false*/
-        }
-        if (ko.isWriteableObservable(value.values)) {
-            $(element).on('slidechange.ko', function () {
-                var values = $(element).slider('values');
-                value.values(values);
-            });
         }
 
         //handle disposal
@@ -39,4 +36,4 @@
         events: ['create', 'start', 'slide', 'change', 'stop'],
         postInit: postInit
     });
-}());
+} ());
