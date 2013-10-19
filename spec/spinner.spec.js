a4aac46a8b780acfd6175f2f04d661d97c57cae7
Vas Gábor <gvas.munka@gmail.com>
2013-10-20 00:56:51 +0200

    Fixed the failing spinner spec.

--- spec/spinner.spec.js
+++ spec/spinner.spec.js
@@ -1,5 +1,5 @@
-/*jslint maxlen:256*/
-/*global ko, $, jasmine, describe, it, beforeEach, afterEach, spyOn, expect, getMajorMinorVersion, testWidgetOptions*/
+/*jslint maxlen:256, browser:true*/
+/*global ko, $, jasmine, describe, it, beforeEach, afterEach, spyOn, expect, runs, waitsFor, getMajorMinorVersion, testWidgetOptions*/
 (function () {
     'use strict';
 
@@ -37,10 +37,10 @@
         });
 
         it('should write the widget\'s value to the viewmodel\'s bound property when it changes.', function () {
-            var $element, vm;
+            var $element, flag = false, vm;
 
             $element = $('<input data-bind="spinner: { value: value }" />').appendTo('body');
-            
+
             vm = { value: ko.observable(1) };
             ko.applyBindings(vm, $element[0]);
 
@@ -49,29 +49,50 @@
             expect(vm.value.peek()).toEqual(55);
             $element.spinner('stepUp');
             expect(vm.value.peek()).toEqual(56);
-            
-            // The spinner behaves a little bit differently than programmatically calling stepUp/stepDown when the user actually clicks up/down.
-            //
-            $element.focus().spinner("widget").find(".ui-spinner-up").mousedown().mouseup();
-            expect($element.spinner('value')).toEqual(57);
-            expect(vm.value.peek()).toEqual(56); // Focus is still on the input, so the spinner hasn't fired spinchange yet.
-            $element.blur().blur(); // Yes, the double blur is necessary.
-            expect(vm.value.peek()).toEqual(57); // *Now* the observable should be mutated.
 
-            ko.removeNode($element[0]);
+            // This spec tests that the view model's bound property is only updated when
+            // the spinner's input is blurred.
+            // The spinner widget ensures that the focus is set to the input when the up-
+            // or down button is clicked, but IE raises the focus event asynchronously,
+            // so we also have to test it asynchronously.
+            runs(function () {
+                $element.spinner('widget').find('.ui-spinner-up')
+                    .mousedown()
+                    .mouseup();
+                setTimeout(function () {
+                    expect($element.spinner('value')).toEqual(57);
+                    expect(vm.value.peek()).toEqual(56); // Focus is still on the input, so the spinner hasn't fired spinchange yet.
+                    $element.blur();
+                    flag = true; // open the latch
+                }, 50);
+            });
+
+            waitsFor(function () {
+                return flag;
+            }, 'latch wasn\'t opened', 1000);
+
+            runs(function () {
+                expect(vm.value.peek()).toEqual(57); // *Now* the observable should be mutated.
+
+                ko.removeNode($element[0]);
+            });
         });
 
         it('should write the widget\'s value immediately to the viewmodel\'s bound property when it changes if "valueUpdate" binding is also used on the input.', function () {
             var $element, vm;
-            
+
             $element = $('<input data-bind="valueUpdate: \'afterkeydown\', spinner: { value: value }" />').appendTo('body');
-            
+
             vm = { value: ko.observable(100) };
             ko.applyBindings(vm, $element[0]);
-            $element.focus().spinner("widget").find(".ui-spinner-up").mousedown().mouseup();
+            $element.spinner('widget').find('.ui-spinner-up')
+                .mousedown()
+                .mouseup();
             expect(vm.value.peek()).toEqual(101);
+
+            ko.removeNode($element[0]);
         });
-        
+
         it('should write the element to the widget observable', function () {
             var $element, vm, disabled;
 
