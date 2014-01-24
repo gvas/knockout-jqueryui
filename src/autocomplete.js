6cb4cc5d5ddadf8ca2cfdabc85250f3d80c2cf1b
Donald Mull Jr <donald.mull@gmail.com>
2014-01-24 12:57:32 -0500

    initial test of adding value bindings

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -1,8 +1,8 @@
-/*global bindingFactory, versions*/
+/*global $, bindingFactory, versions*/
 (function () {
     'use strict';
 
-    var events;
+    var events, postInit;
 
     switch (versions.jQueryUI) {
     case '1.8':
@@ -15,10 +15,47 @@
         break;
     }
 
+    postInit = function (element, valueAccessor) {
+        /// <summary>Adds a binding for selectedItem, selectedValue, selectedLabel
+        /// </summary>
+        /// <param name='element' type='DOMNode'></param>
+        /// <param name='valueAccessor' type='Function'></param>
+
+        var value = valueAccessor();
+
+        if (value.selectedItem) {
+            if (ko.isWriteableObservable(value.selectedItem)) {
+                $(element).on('autocompleteselect', function (evt, ui) {
+                    value.selectedItem(ui.item);
+                    return false;
+                });
+            }
+        }
+
+        if (value.selectedValue) {
+            if (ko.isWriteableObservable(value.selectedValue)) {
+                $(element).on('autocompleteselect', function (evt, ui) {
+                    value.selectedValue(ui.item.value);
+                    return false;
+                });
+            }
+        }
+
+        if (value.selectedLabel) {
+            if (ko.isWriteableObservable(value.selectedLabel)) {
+                $(element).on('autocompleteselect', function (evt, ui) {
+                    value.selectedLabel(ui.item.label);
+                    return false;
+                });
+            }
+        }
+    };
+
     bindingFactory.create({
         name: 'autocomplete',
         options: ['appendTo', 'autoFocus', 'delay', 'disabled', 'minLength', 'position',
             'source'],
-        events: events
+        events: events,
+        postInit: postInit
     });
 }());
