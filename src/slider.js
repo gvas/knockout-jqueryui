1fb0414ff27718f343b7117d862f617de9c1c8f6
gvas <gvas.munka@gmail.com>
2014-05-18 21:50:09 +0200

    Fixed the slider binding's animation bug (#28)

--- src/slider.js
+++ src/slider.js
@@ -2,7 +2,9 @@
 (function () {
     'use strict';
 
-    var postInit;
+    var domDataKey, postInit;
+
+    domDataKey = '__kojqui_options';
 
     postInit = function (element, valueAccessor) {
         /// <summary>Keeps the value and the values binding property in sync with the
@@ -20,6 +22,14 @@
             $(element).on(changeEvent + '.slider', function (ev, ui) {
                 var index = $(element).find('.ui-slider-handle').index(ui.handle);
                 if (index === 0) {
+                    // The slider widget, in its _slide() method, raises the
+                    // slide/slidechange events, then immediately updates its value
+                    // property. If any of the hooked event handlers sets the widget's
+                    // value property, it will ruin the sliding animation.
+                    // To prevent this, we make the update() method defined in the
+                    // bindingFactory to believe that the new value is already set on the
+                    // widget.
+                    ko.utils.domData.get(element, domDataKey).value = ui.value;
                     value.value(ui.value);
                 }
             });
@@ -28,6 +38,8 @@
         if (ko.isWriteableObservable(value.values)) {
             /*jslint unparam:true*/
             $(element).on(changeEvent + '.slider', function (ev, ui) {
+                // see the explanation above
+                ko.utils.domData.get(element, domDataKey).value = ui.values;
                 value.values(ui.values);
             });
             /*jslint unparam:false*/
