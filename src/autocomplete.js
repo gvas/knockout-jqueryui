ed82770540513e9a84583b1df3357bafd802e995
Vas Gábor <gvas.munka@gmail.com>
2014-08-13 14:00:30 +0200

    Autocomplete has the 'messages' option only in jQuery UI >= 1.9 Modified jasmine-jsreporter because the SauceLabs tests failed with Chrome

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -16,13 +16,14 @@ define(
 
             BindingHandler.call(this, 'autocomplete');
 
-            this.options = ['appendTo', 'autoFocus', 'delay', 'disabled', 'messages', 'minLength',
+            this.options = ['appendTo', 'autoFocus', 'delay', 'disabled', 'minLength',
                 'position', 'source'];
 
             if (utils.uiVersion.major === 1 && utils.uiVersion.minor === 8) {
                 this.events = ['change', 'close', 'create', 'focus', 'open', 'search',
                     'select'];
             } else {
+                this.options.push('messages');
                 this.events = ['change', 'close', 'create', 'focus', 'open', 'response',
                     'search', 'select'];
             }
